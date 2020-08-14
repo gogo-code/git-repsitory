e15aaeb14148e4e7e71fedef0a04be5450f5d89f
gogo-code <3059349417@qq.com>
2020-08-14 19:13:49 +0800

    小案例

---
+++ 每日前端小练习/vue/vue脚手架/tabbar/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+  @import url(assets/css/base.css);
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+    position:fixed;
+    right:0;
+    bottom: 0;
+    left: 0;
+    box-shadow: 0 -1px 1px rgba(100,100,100,.1);
+  }
+  .tab-bar-item {
+    flex:1;
+    text-align: center;
+    height: 49px;
+  }
+
+</style>
