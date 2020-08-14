8f39fdeea2fae9323bd8e3175b5c318f38be9ce5
gogo-code <3059349417@qq.com>
2020-08-14 09:08:55 +0800

    全局守卫

--- 每日前端小练习/vue/vue脚手架/test1/src/App.vue
+++ 每日前端小练习/vue/vue脚手架/test1/src/App.vue
@@ -8,7 +8,10 @@
     <button @click="linkToAbout">关于</button> -->
     <router-link to="/home">首页</router-link>
     <router-link to="/about">关于</router-link>
-    <router-link :to="'/user/'+userId">用户</router-link>
+    <!-- <router-link :to="'/user/'+userId">用户</router-link>
+    <router-link :to="{path:'/profile'}">档案</router-link> -->
+    <button @click="userClick">用户</button>
+    <button @click="profileClick">档案</button>
     <router-view></router-view>
   </div>
 </template>
@@ -28,6 +31,19 @@ export default {
     },
     linkToAbout() {
       this.$router.push('/about')
+    },
+    userClick () {
+      this.$router.push('/user/'+this.userId)
+    },
+    profileClick() {
+      this.$router.push({
+        path:'/profile',
+        query:{
+          name:'vobe',
+          age:12,
+          height:1.87
+        }
+      })
     }
   },
 };
--- 每日前端小练习/vue/vue脚手架/test1/src/router/index.js
+++ 每日前端小练习/vue/vue脚手架/test1/src/router/index.js
@@ -8,6 +8,7 @@ const HomeNews = () => import("../components/HomeNews");
 const HomeMessage = () => import("../components/HomeMessage");
 const About = () => import("../components/About");
 const User = () => import("../components/User");
+const Profile = () => import("../components/Profile");
 Vue.use(VueRouter);
 
 const routes = [
@@ -18,34 +19,61 @@ const routes = [
     {
         path: "/home",
         component: Home,
+        meta: {
+            title: "首页",
+        },
         children: [
             {
-                path:'',
-                redirect:'news'
+                path: "",
+                redirect: "news",
             },
             {
                 path: "news",
                 component: HomeNews,
+                meta: {
+                    title: "新闻",
+                },
             },
             {
                 path: "message",
                 component: HomeMessage,
+                meta: {
+                    title: "消息",
+                },
             },
         ],
     },
     {
         path: "/about",
         component: About,
+        meta: {
+            title: "关于",
+        },
     },
     {
         path: "/user/:userId",
         component: User,
+        meta: {
+            title: "用户",
+        },
+    },
+    {
+        path: "/profile",
+        component: Profile,
+        meta: {
+            title: "档案",
+        },
     },
 ];
 const router = new VueRouter({
     routes,
     mode: "history",
     linkActiveClass: "active",
+});
+router.beforeEach((to, from, next) => {
+    document.title = to.matched[0].meta.title
+    
+    next();
 });
 
 export default router;
