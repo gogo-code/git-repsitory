df50609e447405ae90236288ef996cbb5fd72676
gogo-code <3059349417@qq.com>
2020-08-16 10:09:39 +0800

    优化更新

--- 每日前端小练习/vue/vue脚手架/tabbar/src/App.vue
+++ 每日前端小练习/vue/vue脚手架/tabbar/src/App.vue
@@ -1,36 +1,24 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+    
   </div>
 </template>
 
 <script>
+
+import MainTabBar from "./components/MainTabBar"
 export default {
   name: "App",
-  components: {},
+  components: {
+
+    MainTabBar
+  },
 };
 </script>
 
 <style>
-  @import url(assets/css/base.css);
-  #tab-bar {
-    display: flex;
-    background-color: #f6f6f6;
-    position:fixed;
-    right:0;
-    bottom: 0;
-    left: 0;
-    box-shadow: 0 -1px 1px rgba(100,100,100,.1);
-  }
-  .tab-bar-item {
-    flex:1;
-    text-align: center;
-    height: 49px;
-  }
+@import url(assets/css/base.css);
 
 </style>
---
+++ 每日前端小练习/vue/vue脚手架/tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="tab-bar">
+  <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
