09ab1c12eed1133129208c97c78b985f61df1cfc
左国梁 <zuoguoliang@ly-sky.com>
2021-07-29 13:55:19 +0800

    12

--- 每日前端小练习/JavaScript/leetcode/Two pointers/977.有序数组的平方.js
+++ 每日前端小练习/JavaScript/leetcode/Two pointers/977.有序数组的平方.js
@@ -9,11 +9,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-// 方法一：暴力破解
+
+const sortedSquares = function (nums) {
+  // 方法一：暴力破解
   let arr=nums.map((value)=>value**2).sort((a,b)=>a-b)
   return arr
+  // 方法二：双指针（性能不太好）
+  // let res = []
+  // for (let i = 0, j = nums.length - 1; i <= j;) {
+  //   const left = Math.abs(nums[i])
+  //   const right = Math.abs(nums[j])
+  //   if (right > left) {
+  //     // push element to the front of the array
+  //     res.unshift(right * right)
+  //     j--
+  //   } else {
+  //     res.unshift(left * left)
+  //     i++
+  //   }
+  // }
+  // return res
+}
 
-};
 // @lc code=end
 
