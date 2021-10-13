b78e11fc4043ba863692cbbf49891ecbcd2e6470
左国梁 <3059349417@qq.com>
2021-10-13 23:29:39 +0800

    帧控制（失败）

--- 每日前端小练习/React/todolist/src/TodoList.js
+++ 每日前端小练习/React/todolist/src/TodoList.js
@@ -1,17 +1,70 @@
-import React, { Component, Fragment } from 'react';
-import './style.css';
-import TodoItem from './TodoItem';
+import React, { Component, Fragment } from "react";
+import "./style.css";
+import TodoItem from "./TodoItem";
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: '',
+      inputValue: "",
       list: [],
     };
-    this.handleBtnClick=this.handleBtnClick.bind(this)
-    this.handleInputChange=this.handleInputChange.bind(this)
-    this.handleItemDelete=this.handleItemDelete.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
+
+  getTitle = (title) => {
+    let word = title.split("");
+    return (
+      <div ref={(ele) => (this.div = ele)}>
+        {word.map((val, i) => (
+          <span
+            style={{
+              opacity: 0,
+              // transition: " all 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) 0s",
+              display: "inline-block",
+              transform: "translate(26px,26px)",
+            }}
+          >
+            {val}
+          </span>
+        ))}
+      </div>
+    );
+  };
+
+  show = (params) => {
+    let childList = this.div.childNodes;
+    console.log(this.div.childNodes);
+    for (let i = 0; i < childList.length; i++) {
+      let delay = (i + 1) * 0.1;
+      // childList[i].style.transitionDelay = `${delay}s`;
+      let translateCount = 26;
+      let opacityCount = 0;
+      let timeHandle = null;
+      this.animation(i, childList, translateCount, timeHandle, opacityCount);
+    }
+  };
+
+  animation = (i, childList, translateCount, timeHandle, opacityCount) => {
+    let _opacityCount = opacityCount;
+    let _translateCount = translateCount;
+    if (_opacityCount >= 0.84) {
+      childList[i].style.transform = `translate(0px,0px)`;
+      childList[i].style.opacity = 1;
+      return;
+    }
+    _opacityCount += 0.01;
+    _translateCount -= 0.44;
+    childList[
+      i
+    ].style.transform = `translate(${_translateCount}px,${_translateCount}px)`;
+    childList[i].style.opacity = _opacityCount;
+    setTimeout(() => {
+      this.animation(i, childList, _translateCount, timeHandle, _opacityCount);
+    }, 16.7);
+  };
+
   render() {
     return (
       <Fragment>
@@ -23,22 +76,45 @@ class TodoList extends Component {
             className="input"
             value={this.state.inputValue}
             onChange={this.handleInputChange}
-            ref={(input)=>{this.input=input}}
+            ref={(input) => {
+              this.input = input;
+            }}
           ></input>
           <button onClick={this.handleBtnClick}>提交</button>
         </div>
-        <ul 
-        ref={(ul)=>{this.ul=ul}}>
+        <ul
+          ref={(ul) => {
+            this.ul = ul;
+          }}
+        >
           {this.getTodoItem()}
         </ul>
+
+        <div
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            height: 26,
+            overflow: "hidden",
+          }}
+        >
+          {this.getTitle("我是你爹")}
+        </div>
+        <button onClick={() => this.show(4)} style={{ marginTop: 10 }}>
+          显示
+        </button>
       </Fragment>
     );
   }
-  getTodoItem(){
+  getTodoItem() {
     return this.state.list.map((item, index) => {
       return (
         <div key={index}>
-          <TodoItem  content={item} index={index} deleteItem={this.handleItemDelete}/>
+          <TodoItem
+            content={item}
+            index={index}
+            deleteItem={this.handleItemDelete}
+          />
         </div>
       );
       //  <li
@@ -46,28 +122,29 @@ class TodoList extends Component {
       // onClick={this.handleItemDelete.bind(this,index)}
       // dangerouslySetInnerHTML={{__html:item}}
       // ></li>
-    })
+    });
   }
   handleInputChange(e) {
     // this.state.inputValue=e.target.value;
     // const value=e.target.value
-    const value=this.input.value
-    this.setState(()=>({
-      
-        inputValue: value,
-      
-    }))
-    
+    const value = this.input.value;
+    this.setState(() => ({
+      inputValue: value,
+    }));
+
     console.log(e.target.value);
   }
   handleBtnClick() {
     // prevState相当于this.state
-    this.setState((prevState)=>({
-      
+    this.setState(
+      (prevState) => ({
         list: [...prevState.list, prevState.inputValue],
-        inputValue: ''
-      
-    }),()=>{console.log(this.ul.querySelectorAll('div').length)});
+        inputValue: "",
+      }),
+      () => {
+        console.log(this.ul.querySelectorAll("div").length);
+      }
+    );
   }
   handleItemDelete(index) {
     // imutable
@@ -75,13 +152,11 @@ class TodoList extends Component {
     // 把数组做一份拷贝
     // const list = [...this.state.list];
     // list.splice(index, 1);
-    this.setState(
-      (prevState)=>{
-        const list=[...prevState.list]
-        list.splice(index,1)
-        return {list}
-      }
-    );
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return { list };
+    });
   }
 }
 export default TodoList;
